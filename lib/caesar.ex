15c79ce7a1036e89d0170116ff34f8374e760d91
Nate West <natescott.west@gmail.com>
2014-06-19 21:00:11 -0500

    Encipher

--- lib/caesar.ex
+++ lib/caesar.ex
@@ -1,8 +1,17 @@
 defmodule Caesar do
+  def encipher(string, key) do
+    { m, s } = key
+    cipher(string, cipher_func(m, s))
+  end
+
   def cipher(string, func) do
     string |> String.downcase
            |> String.to_char_list
            |> Enum.map(func)
            |> List.to_string
+  end
+
+  def cipher_func(m, s) do
+    fn(c) -> rem((c * m + s), 25) end
   end
 end
--- test/caesar_test.exs
+++ test/caesar_test.exs
@@ -5,4 +5,8 @@ defmodule CaesarTest do
     assert Caesar.cipher("hi", fn(c) -> c + 3 end) == "kl"
     assert Caesar.cipher("kl", fn(c) -> c - 3 end) == "hi"
   end
+
+  test "encipher" do
+    assert Caesar.encipher("hi", { 3, 2 }) == <<14, 17>>
+  end
 end
