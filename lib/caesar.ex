ee1989300302e0a3eca160df5b27839403600bef
Nate West <natescott.west@gmail.com>
2014-06-20 18:29:42 -0500

    Fix syntax error

--- lib/caesar.ex
+++ lib/caesar.ex
@@ -27,7 +27,7 @@ defmodule Caesar do
     fn(c) -> rem((c * m + s), 127) end
   end
 
-  def valid_primes
+  def valid_primes do
   [
     2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
     31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
